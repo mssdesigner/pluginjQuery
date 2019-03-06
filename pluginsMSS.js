876daf564b5050503d4f4f455c31b59f6f9bc434
Marcelo dos Santos Silva <mssdesigner@gmail.com>
2019-03-05 23:17:45 -0300

    Add files via upload

--- pluginsMSS.js
+++ pluginsMSS.js
@@ -41,54 +41,80 @@
     },
 
     createDropdown: function(params, selected, isOptionDefault = true) {
-        var optionsDefault = [{ id: 1, label: "Selecione" }],
-          opc = $.extend(optionsDefault, params);
-  
-        function start() {
-          function create(element) {
-            this.div = $("<div/>", {
-              class: "dropdown"
+      var optionsDefault = [{ id: 1, label: "Selecione" }],
+        opc = $.extend(optionsDefault, params);
+
+      function start() {
+        function create(element) {
+          this.div = $("<div/>", {
+            class: "dropdown"
+          });
+
+          this.btn = $("<button/>", {
+            class: "btn btn-primary dropdown-toggle",
+            type: "button",
+            "data-toggle": "dropdown",
+            html: isOptionDefault ? "Selecione" : ""
+          });
+
+          this.span = $("<span/>", { class: "caret" });
+          this.ul = $("<ul/>", { class: "dropdown-menu" });
+          this.btn.appendTo(this.div);
+          this.span.appendTo(this.div);
+          this.ul.appendTo(this.div);
+
+          opc.forEach(function(e) {
+            this.a = $("<a/>", {
+              id: e.id,
+              href: "javascript:void(0)",
+              html: e.label,
+              onclick: "updateLabel(this)"
             });
-  
-            this.btn = $("<button/>", {
-              class: "btn btn-primary dropdown-toggle",
-              type: "button",
-              "data-toggle": "dropdown",
-              html: isOptionDefault ? "Selecione" : ""
-            });
-  
-            this.span = $("<span/>", { class: "caret" });
-            this.ul = $("<ul/>", { class: "dropdown-menu" });
-            this.btn.appendTo(this.div);
-            this.span.appendTo(this.div);
-            this.ul.appendTo(this.div);
-  
-            opc.forEach(function(e) {
-              this.a = $("<a/>", {
-                id: e.id,
-                href: "javascript:void(0)",
-                html: e.label,
-                onclick: "updateLabel(this)"
-              });
-  
-              this.li = $("<li/>");
-              this.a.appendTo(this.li);
-              this.li.appendTo(this.ul);
-            });
-  
-            var newLabel = div.find("a").filter(function(i, e) {
-              return e.id == selected;
-            });
-            this.div.find("button").html(newLabel.html());
-            newLabel.parent().remove();
-            element.append(this.div[0]);
-          }
-  
-          create(this);
+
+            this.li = $("<li/>");
+            this.a.appendTo(this.li);
+            this.li.appendTo(this.ul);
+          });
+
+          var newLabel = div.find("a").filter(function(i, e) {
+            return e.id == selected;
+          });
+          this.div.find("button").html(newLabel.html());
+          newLabel.parent().remove();
+          element.append(this.div[0]);
         }
-  
-        return $(this).each(start);
+        create(this);
       }
+
+      return $(this).each(start);
+    },
+
+    createInput: function(params) {
+      var optionsDefault = { type: "text", placeholder: "", id: "", value: "" },
+        opc = $.extend(optionsDefault, params );
+      function start() {
+        function create(element) {
+          this.div = $("<div/>", {
+            class: ""
+          });
+
+          this.input = $("<input/>", {
+            class: "form-control",
+            type: opc.type,
+            placeholder: opc.placeholder,
+            id: opc.id,
+            name: opc.id,
+            html: opc.value
+          });
+
+          this.input.appendTo(this.div);
+          element.append(this.div[0]);
+        }
+
+        create(this);
+      }
+      return $(this).each(start);
+    }
   });
 })(jQuery);
 
@@ -100,19 +126,19 @@ function getRow(e) {
 }
 
 function updateLabel(e, selected) {
-    this.newLabel = e.innerText;
-    this.oldLabel = e.parentNode.parentNode.parentNode.getElementsByTagName(
-      "button"
-    )[0].innerText;
-    // this.oldLabel = $("div[id*='component-dropdown']").find('button').html();
-    e.innerText = this.oldLabel;
-    e.parentNode.parentNode.parentNode.getElementsByTagName(
-      "button"
-    )[0].innerText = this.newLabel;
-    var listLi = e.parentNode.parentNode.getElementsByTagName("li");
-    for (var i = 0; i < listLi.length; i++) {
-      if (listLi[i].innerText === "Selecione") {
-        listLi[i].remove();
-      }
+  this.newLabel = e.innerText;
+  this.oldLabel = e.parentNode.parentNode.parentNode.getElementsByTagName(
+    "button"
+  )[0].innerText;
+  // this.oldLabel = $("div[id*='component-dropdown']").find('button').html();
+  e.innerText = this.oldLabel;
+  e.parentNode.parentNode.parentNode.getElementsByTagName(
+    "button"
+  )[0].innerText = this.newLabel;
+  var listLi = e.parentNode.parentNode.getElementsByTagName("li");
+  for (var i = 0; i < listLi.length; i++) {
+    if (listLi[i].innerText === "Selecione") {
+      listLi[i].remove();
     }
   }
+}
